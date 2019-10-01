228130fbeec0082f28f14b27a71d529c817157d5
Unicorn User <nonexistingemail@gmail.com>
2019-10-01 11:17:38 +0300

    turtle with carrot

--- scripts/turtle_tf_broadcaster_carrot.py
+++ scripts/turtle_tf_broadcaster_carrot.py
@@ -4,6 +4,7 @@ import rospy
 import tf
 from tf.transformations import quaternion_from_euler
 from turtlesim.msg import Pose
+import math 
 
 # Register node / fake node name - we will rename =)
 rospy.init_node('tf_turtle')
@@ -13,6 +14,7 @@ turtlename = rospy.get_param('~turtle_tf_name')
 def handle_turtle_pose(msg):
     # Get broadcaster object
     br = tf.TransformBroadcaster()
+
     # Broadcast TF trasform (world -> turtlename)
     br.sendTransform((msg.x, msg.y, 0),
                      quaternion_from_euler(0, 0, msg.theta),
@@ -20,10 +22,25 @@ def handle_turtle_pose(msg):
                      turtlename,
                      "world")
 
+def handle_carrot_pose(msg):
+    carrot = tf.TransformBroadcaster()
+    omega = 100
+    info = Pose()
+    now = rospy.Time.now()
+    carrot_x = info.x + 0.5 * math.cos(omega * now.nsecs)
+    carrot_y = info.y + 0.5 * math.sin(omega * now.nsecs)
+
+    carrot.sendTransform((carrot_x, carrot_y, 0),
+                         quaternion_from_euler(0, 0, info.theta),
+                         rospy.Time.now(),
+                         "carrot",
+                         turtlename
+        )
+
 # Subscribe to /input_pose topic - just gonna remap it to work with
 rospy.Subscriber('input_pose',
                  Pose,
-                 handle_turtle_pose)
+                 handle_carrot_pose)
 # You spin my head right round, right round - Florida =)
 # Just handle all topic messages until node (or ROS) is working
 rospy.spin()
